e699809f8ade69c9a615be7226f2549bf4e3f3dd
anukkrit149 <anukkrit.179301038@muj.manipal.edu>
2020-09-14 07:48:55 +0530

    Final App

---
+++ stories/urls.py
@@ -0,0 +1,12 @@
+"""
+Copyrights Reserved 
+Developed By- Anukkrit Shanker
+"""
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', index, name='home'),
+    path('story/<int:pk>', storyDetail),
+    path('story/<int:pk>/<int:userId>/api/', storyLiveViews.as_view())
+]
---
+++ users/urls.py
@@ -0,0 +1,14 @@
+"""
+Copyrights Reserved 
+Developed By- Anukkrit Shanker
+"""
+from django.urls import path
+from .views import *
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+
+    path('register/', signup, name='Signup'),
+    path('', auth_views.LoginView.as_view(template_name='detail/login.html'), name='Login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='detail/login.html'), name='Logout'),
+]
